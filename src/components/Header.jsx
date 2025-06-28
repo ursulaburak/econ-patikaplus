e361eee62e9f17dbb31cd3250bcb042a9a0469a9
Burak Avcılar <burakkavcilar@gmail.com>
2025-06-28 23:17:43 +0300

    Add files via upload

---
+++ src/components/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Header.css'
+
+const Header = () => {
+  return (
+        <header>
+            <nav>
+                <div className='navbar-logo'>
+                    <img src="./logo.svg" alt="logo" />
+                </div>
+            </nav>
+        </header>
+  )
+}
+
+export default Header
---
+++ src/components/Hero.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Hero.css'
+
+const Hero = () => {
+  return (
+    <div className='section'>
+      <div className='hero-container'>
+          <img id='billgates' src="./billgates.jpg" alt="Bill Gates" />
+          <h1>Spend Bill Gates Money</h1>
+        </div>
+    </div>
+  )
+}
+
+export default Hero
