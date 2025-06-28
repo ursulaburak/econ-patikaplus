cc1c9c916df1da687c819f6380d419fb857e2d79
Burak Avcılar <burakkavcilar@gmail.com>
2025-06-28 16:37:29 +0300

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,21 +1,16 @@
 import React from 'react'
-import { Container, Row, Col, Image } from 'react-bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
 import './Header.css'
 
-function Header() {
+const Header = () => {
   return (
-    <Container className='mx-auto '>
-      <Row className="mt-3 bg-white">
-        <Col className='pt-3'>
-          <Image src='https://neal.fun/spend/billgates.jpg' alt='Bill Gates' roundedCircle style={{ width: 125, height: 125 }} className='rounded mx-auto d-block img mt-4' />
-          <p className="text-center mt-4 title">
-            Spend Bill Gates' Money
-          </p>
-        </Col>
-      </Row>
-    </Container>
+        <header>
+            <nav>
+                <div className='navbar-logo'>
+                    <img src="./logo.svg" alt="logo" />
+                </div>
+            </nav>
+        </header>
   )
 }
 
-export default React.memo(Header);
+export default Header
