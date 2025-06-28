cf8079686de6eb192842ab56cf454f30a7ff339f
Burak Avcılar <burakkavcilar@gmail.com>
2025-06-28 16:15:57 +0300

    Delete src/redux directory

--- src/redux/store.js
+++
@@ -1,9 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-
-import spendSlice from "./spend/spendSlice";
-
-export const store = configureStore({
-    reducer: {
-        spend: spendSlice,
-    },
-});
