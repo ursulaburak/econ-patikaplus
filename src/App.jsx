86d7d650e65a6c2474d4ecbbcf0a7879602a6751
Burak Avcılar <burakkavcilar@gmail.com>
2025-06-28 16:40:59 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,11 @@ import { useState } from 'react'
 import './App.css'
 import Header from './components/Header.css'
 import Receipt from './components/Receipt.css'
-import SpendMoney from './components/SpendMoney.css'
+import SpendMoney from './components/Products.css'
 import Footer from './components/Footer.css'
 import Header from './components/Header.js'
 import Receipt from './components/Receipt.js'
-import SpendMoney from './components/SpendMoney.js'
+import SpendMoney from './components/Products.js'
 import Footer from './components/Footer.js'
 
 function App() {
