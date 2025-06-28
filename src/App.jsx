489fe26331da42a3c0ee462b17e2c3d19fd1c6cb
Burak Avcılar <burakkavcilar@gmail.com>
2025-06-28 16:17:19 +0300

    Update and rename App.js to App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import './App.css'
+import Header from './components/Header/Header'
+import Hero from './components/Hero/Hero'
+import Products from './components/Products/Products'
+import Footer from './components/Footer/Footer'
+
+function App() {
+
+
+  return (
+    <>
+      <Header />
+      <Hero />
+      <Products />
+      <Footer />
+    </>
+  )
+}
+
+export default App
