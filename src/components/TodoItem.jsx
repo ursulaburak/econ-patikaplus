e471c5a0d8a0654526f2e161f1882ff0a0bb5479
Burak Avcılar <burakkavcilar@gmail.com>
2025-07-09 16:50:01 +0300

    Add files via upload

---
+++ src/components/TodoItem.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const TodoItem = ({ todo, toggleTodo, removeTodo }) => {
+  return (
+    <li className={todo.completed ? 'completed' : ''}>
+      <div className="view">
+        <input
+          className="toggle"
+          type="checkbox"
+          checked={todo.completed}
+          onChange={() => toggleTodo(todo.id)}
+        />
+        <label>{todo.text}</label>
+        <button className="destroy" onClick={() => removeTodo(todo.id)}></button>
+      </div>
+    </li>
+  );
+};
+
+export default TodoItem;
